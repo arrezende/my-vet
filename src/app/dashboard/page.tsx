eed84f894025d18d6bf841655d8c4f0df0fec9bb
Alex Rodrigo Rezende <arrezende@gmail.com>
2025-01-03 09:11:57 -0300

    feat: add card components and area chart for dashboard statistics

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,52 +1,128 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { Header } from '@/components/header'
+import { ChartArea } from './chart/area'
 import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from '@/components/ui/card'
 
-export default function Page() {
+import Link from 'next/link'
+
+const animalMonth = [
+  { month: 'Janeiro', quantity: 186 },
+  { month: 'Fevereiro', quantity: 305 },
+  { month: 'Março', quantity: 237 },
+  { month: 'Abril', quantity: 73 },
+  { month: 'Maio', quantity: 209 },
+  { month: 'Junho', quantity: 214 },
+]
+const consultsMonth = [
+  { month: 'Janeiro', quantity: 10 },
+  { month: 'Fevereiro', quantity: 20 },
+  { month: 'Março', quantity: 50 },
+  { month: 'Abril', quantity: 5 },
+  { month: 'Maio', quantity: 60 },
+  { month: 'Junho', quantity: 214 },
+]
+const lastAnimals = [
+  { month: 'Bella', desktop: 186 },
+  { month: 'Max', desktop: 305 },
+  { month: 'Luna', desktop: 237 },
+]
+
+export default async function OwnerList() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+    <>
+      <Header category="Home" link="/" page="Dashboard" />
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+          <div>
+            <h3 className="text-lg font-medium">Dashboard</h3>
+            <p className="text-sm text-muted-foreground mb-4 text-zinc-600">
+              Bem-vindo ao seu painel de controle, onde você pode visualizar e
+              gerenciar todas as suas informações de forma eficiente.
+            </p>
           </div>
-        </header>
-        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
-            <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="h-px bg-zinc-300 mb-4"></div>
+          <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+            <ChartArea
+              data={animalMonth}
+              title="Animais por mês"
+              description="Mostrando o total de animais nos últimos 6 meses"
+            />
+            <Card>
+              <CardHeader>
+                <CardTitle>Últimos Atendimentos (24h)</CardTitle>
+                <CardDescription>
+                  Mostrando o total de atendimentos nas últimas 24h
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul>
+                  <li>
+                    <Link href="#">Vale Nada</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Tostão</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Miau</Link>
+                  </li>
+                </ul>
+              </CardContent>
+            </Card>
+            <ChartArea
+              data={consultsMonth}
+              title="Consultas por mês"
+              description="Mostrando o total de consultas nos últimos 6 meses"
+            />
+            <Card>
+              <CardHeader>
+                <CardTitle>Últimos Atendimentos (24h)</CardTitle>
+                <CardDescription>
+                  Mostrando o total de atendimentos nas últimas 24h
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul>
+                  <li>
+                    <Link href="#">Vale Nada</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Tostão</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Miau</Link>
+                  </li>
+                </ul>
+              </CardContent>
+            </Card>{' '}
+            <Card>
+              <CardHeader>
+                <CardTitle>Últimos animais cadastrados (24h)</CardTitle>
+                <CardDescription>
+                  Mostrando o total de animais cadastrados nas últimas 24h
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <ul>
+                  <li>
+                    <Link href="#">Vale Nada</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Tostão</Link>
+                  </li>
+                  <li>
+                    <Link href="#">Miau</Link>
+                  </li>
+                </ul>
+              </CardContent>
+            </Card>
           </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+    </>
   )
 }
